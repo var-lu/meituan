ff5ce654328745e1a2afefa5002b9c3cd6073b99
var-lu <1061709310@qq.com>
2019-04-30 10:59:29 +0800

    正式版本1.0
    Signed-off-by: var-lu <1061709310@qq.com>

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-
+import router from './router/index'
+import store from "@/store/index"
+import axios from "axios"
 Vue.config.productionTip = false
-
+Vue.prototype.$axios=axios;
 new Vue({
   router,
+  store,
   render: function (h) { return h(App) }
 }).$mount('#app')
