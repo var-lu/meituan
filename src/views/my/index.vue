410eaacae8549ef4c5faf304920bb546a3faaff5
var-lu <1061709310@qq.com>
2019-04-30 15:52:48 +0800

    添加VIEW
    Signed-off-by: var-lu <1061709310@qq.com>

---
+++ src/views/my/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="my">
+        我的页面
+    </div>
+</template>
+<script>
+export default {
+    name:"my"
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/views/order/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="order">
+    <h1>订单页</h1>
+  </div>
+</template>
+<script>
+export default {
+  name:"order"
+}
+</script>
+<style scoped>
+
+</style>
+
