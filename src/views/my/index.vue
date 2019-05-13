3b7719d1d1069610e7b1660c57869e402f3334bc
var-lu <1061709310@qq.com>
2019-05-13 22:09:35 +0800

    vue.config
    Signed-off-by: var-lu <1061709310@qq.com>

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,7 +1,21 @@
 <template>
     <div class="my">
-        我的页面
-        
+        <section class="my_header">
+            <div><img src="../../assets/img/head-defult.png" alt=""></div>
+            <p>测试名</p>
+        </section>
+        <section class="my_nav">
+            <ul>
+                <li class="red_package">美团红包</li>
+                <li class="red_package">收货地址</li>
+                <li class="red_package">常见问题</li>
+                <li class="red_package">美团协议与说明</li>
+                <li class="red_package">退出登录</li>
+            </ul>
+        </section>
+        <section class="my_phone">
+
+        </section>
     </div>
 </template>
 <script>
@@ -14,5 +28,36 @@ export default {
         width: 100%;
         height: 100vh;
         background: rgb(240, 240, 240);
+        .my_header{
+            width: 100%;
+            padding: 0.37rem 0 0.18rem;
+            background: url(../../assets/img/my-header-bg.png);
+            background-size: 100% 100%;
+            div{
+                img{
+                    width: 100%;
+                    height: 100%;
+                }
+                width: 0.74rem;
+                height: 0.74rem;
+                border-radius: 100%;
+                border: 0.04rem solid #ffe699;
+                background: url(../../assets/img/head-defult.png) no-repeat ;
+                background-size: 100% 100%;
+                margin: auto;
+                
+            }
+            p{
+                text-align: center;
+                font-size: 16px;
+                margin-top: 0.08rem;
+                cursor: pointer;
+                color: #333333;
+                max-width: 100%;
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+            }
+        }
     }
 </style>
