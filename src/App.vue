c2faa4edf2c81df09f0db9545340ff629414fd20
Esther-JJ <yuan125809@163.com>
2019-05-14 20:47:54 +0800

    更改底部公共部分控制
    Signed-off-by: Esther-JJ <yuan125809@163.com>

--- src/App.vue
+++ src/App.vue
@@ -4,25 +4,32 @@
      <!-- <router-link to="/shop">
             <div>店铺</div>
       </router-link>  -->
-    <footNav v-if="footNavisShow"></footNav>
+     <!--<footNav v-if="footNavisShow"></footNav>-->
+     <!--直接切入路径  屏蔽底部公共部分-->
+     <footNav v-if= "(path==='/my' || path==='/order' || path==='/')"></footNav>
   </div>
 </template>
 <script>
 import "@/assets/sass/style.scss"
   export default {
-      name : "App",
+    name : "App",
     data(){
       return{
-        footNavisShow:true
+        //footNavisShow:true,
+        path:""
       }
+    },
+    mounted(){
+    	this.path = this.$route.path;
     },
     watch:{
       $route(to,from){
-        if(to.path === "/shop"){
-          this.footNavisShow = false;
-        }else{
-          this.footNavisShow = true;
-        }
+//      if(to.path === "/shop" || to.path === "/orderpreview"){
+//        this.footNavisShow = false;
+//      }else{
+//        this.footNavisShow = true;
+//      }
+				this.path = to.path;
       }
     }
   }
